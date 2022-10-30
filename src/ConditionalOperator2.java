2ef9381f5cac5ad244361ffb837f15c421962960
harlamenkoel <harlamenkoel@gmail.com>
2022-10-30 23:11:13 +0700

    Начало домашней работы условные операторы Часть 2 Задание 1

--- src/ConditionalOperator2.java
+++ src/ConditionalOperator2.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class ConditionalOperator2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Задание 1");
 
-        System.out.println("Hello world!");
+        System.out.println("Ваша операционная система:\n0.iOS;\n1.Android");
+        System.out.print("Выберите свою: ");
+        int clientOS = scanner.nextInt();
+        switch (clientOS) {
+            case 0 -> System.out.println("«Установите версию приложения для iOS по ссылке»");
+            case 1 -> System.out.println("«Установите версию приложения для Android по ссылке»");
+        }
     }
 }
